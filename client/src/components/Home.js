a33bec9e928a4a830f049b2b518d24ae65514695
unknown <hetarth002@gmail.com>
2023-08-02 11:36:37 +0530

    create peerID and network

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,33 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Home = () => {
-  const [name, setName] = useState("");
-  const [join, setJoin] = useState("");
+  const [id, setId] = useState("");
+  const [servers, setServers] = useState([]);
 
-  const handleNameChange = (e) => {
-    setName(e.target.value);
+  const getNetworks = async () => {
+    axios
+      .get("http://localhost:5000/servers")
+      .then((res) => setServers(res.data))
+      .catch((err) => console.log(err));
   };
-  const handleTopicChange = (e) => {
-    setJoin(e.target.value);
+  const getPeerId = () => {
+    axios
+      .get("http://localhost:5000/id")
+      .then((res) => setId(res.data))
+      .catch((err) => console.log(err));
+  };
+  const createNetwork = async () => {
+    try {
+      axios.post("http://localhost:5000/create", id);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  const joinNetwork = (e) => {
-    e.preventDefault();
-    const cred = { name, join };
-    axios.post("http://localhost:5000/connect", cred);
-    console.log(cred);
+  const handleIdChange = (e) => {
+    setId(e.target.value);
   };
+
   return (
-    <div>
-      <h1>P2P Chat App using Holepunch</h1>
-      <form onSubmit={joinNetwork}>
-        <label>Enter your name</label>
-        <input onChange={handleNameChange} />
-        <label>Enter topic</label>
-        <input onChange={handleTopicChange} />
-        <input type="submit" />
-      </form>
+    <div className="container text-center">
+      <h1 className="m-5">P2P Chat App using Holepunch</h1>
+      <h2>Your peerID: {id}</h2>
+      <input
+        onChange={handleIdChange}
+        className="form-control"
+        placeholder="Enter your PeerID"
+      ></input>
+      <button className="btn btn-outline-primary m-3">Join a network</button>
+      <button onClick={createNetwork} className="btn btn-outline-primary m-3">
+        Create a network
+      </button>
+      <button onClick={getPeerId} className="btn btn-outline-primary m-3">
+        Get a new PeerID
+      </button>
+      <button className="btn btn-outline-primary m-3" onClick={getNetworks}>
+        Get available networks
+      </button>
+      {servers.map((server, index) => {
+        return <div key={index}>{server}</div>;
+      })}
     </div>
   );
 };
--- server/server.js
+++ server/server.js
@@ -1,79 +1,45 @@
 const express = require("express");
 const cors = require("cors");
-const Hyperswarm = require("hyperswarm");
+const DHT = require("hyperdht");
 const goodbye = require("graceful-goodbye");
-const crypto = require("hypercore-crypto");
 const b4a = require("b4a");
+const crypto = require("hypercore-crypto");
+const Hyperswarm = require("hyperswarm");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const swarm = new Hyperswarm();
-goodbye(() => swarm.destroy());
-const connections = new Map(); // Use a Map to store user connections
+const PORT = 5000;
 
-app.post("/connect", (req, res) => {
-  const { name, join } = req.body;
+const servers = [];
+const conn = new Map();
 
-  // Validate the input parameters (e.g., name should not be empty)
-  if (!name || typeof name !== "string" || name.trim() === "") {
-    return res.status(400).json({ error: "Invalid name provided." });
-  }
-
-  // Check if the user is already connected and reject if so
-  if (connections.has(name)) {
-    return res.status(400).json({ error: "User is already connected." });
-  }
-
-  // Generate a unique user ID for the new user
-  const userID = crypto.randomBytes(16).toString("hex");
-
-  // Add the user's connection to the connections Map
-  connections.set(name, { userID, conn: null });
-
-  swarm.on("connection", (conn) => {
-    const user = [...connections.values()].find((u) => u.conn === null);
-    if (user) {
-      user.conn = conn;
-
-      console.log(`* ${name} (${user.userID}) connected *`);
-
-      // Handle data and disconnection events for the user's connection
-      conn.on("data", (data) => {
-        const sender = [...connections.values()].find((u) => u.conn === conn);
-        if (sender) {
-          for (const { conn } of connections.values()) {
-            if (conn !== null && conn !== sender.conn) {
-              conn.write(`${name}: ${data}`);
-            }
-          }
-        }
-      });
-
-      conn.on("close", () => {
-        connections.delete(name);
-        console.log(`* ${name} (${user.userID}) disconnected *`);
-      });
-    }
-  });
-  console.log(connections);
-
-  // Generate the topic based on the provided "join" parameter or a random topic
-  const topic = join ? b4a.from(join, "hex") : crypto.randomBytes(32);
-
-  // Join the P2P network with the generated topic
-  const discovery = swarm.join(topic, { client: true, server: true });
-
-  discovery.flushed().then(() => {
-    console.log(
-      `* ${name} (${userID}) joined topic: ${b4a.toString(topic, "hex")} *`
-    );
-  });
-
-  res.status(200).json({ userID, topic: b4a.toString(topic, "hex") });
+app.get("/id", (req, res) => {
+  const keyPair = DHT.keyPair();
+  res.status(200).json(b4a.toString(keyPair.publicKey, "hex"));
 });
 
-app.listen(5000, () => {
-  console.log("Server running at port 5000");
+app.post("/create", async (req, res) => {
+  const { peerID } = req.body;
+  const dht = new DHT();
+  const keyPair = DHT.keyPair();
+  conn[b4a.toString(keyPair.publicKey, "hex")] = [];
+  const server = dht.createServer((conn) => {
+    console.log("got connection!");
+    process.stdin.pipe(conn).pipe(process.stdout);
+  });
+  await servers.push(b4a.toString(keyPair.publicKey, "hex"));
+  conn[b4a.toString(keyPair.publicKey, "hex")].push(peerID);
+  server.listen(keyPair).then(() => {
+    console.log("listening on:", b4a.toString(keyPair.publicKey, "hex"));
+  });
+  res.status(200).json("Server created");
+});
+
+app.get("/servers", (req, res) => {
+  res.json(servers);
+});
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
